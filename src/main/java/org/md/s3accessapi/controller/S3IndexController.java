4098d4dd189e205494fe8bc6812ed0ff2ff02de9
Cam Dziurgot <camerondziurgot@gmail.com>
2020-01-28 22:08:32 -0500

    add cors annotation to controller

--- src/main/java/org/md/s3accessapi/controller/S3IndexController.java
+++ src/main/java/org/md/s3accessapi/controller/S3IndexController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@CrossOrigin
 @RestController
 public class S3IndexController {
 
